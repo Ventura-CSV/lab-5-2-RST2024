a987eb543d311618d306c7ba5e7f2c01db765439
RST2024 <rtanwani2024@gmail.com>
2024-07-02 18:46:38 -0700

    msg

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
     getinput(num1, num2)
     print('After call function id n1', id(num1))
     print('After call function id n2', id(num2))
-    print(num1, num2)
+    print(num1,num2)
     return num1, num2
 
 
